4f38a633be81d1509a4fa07e44600556daffaa72
Chetan Seth <chetan.seth2015@gmail.com>
2024-07-11 21:27:28 +0530

    adding Lab003

---
+++ src/ex_07072024/Lab004.java
@@ -0,0 +1,45 @@
+package ex_07072024;
+
+public class Lab004
+{
+
+
+    static {
+        System.out.println("hello");
+
+
+        System.exit(0);
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Running Java program with Main Method
+
+   /*public static void main (String[] $$$$$) {
+        int a = 2;
+        int b = 3;
+        if (a > b) {
+            System.out.println("a is greater then b");
+        } else {
+            System.out.println("b is greater then a");
+        }
+        System.out.println("Able to Run Java program without Main Method Successfully ");
+    }*/
+
+
