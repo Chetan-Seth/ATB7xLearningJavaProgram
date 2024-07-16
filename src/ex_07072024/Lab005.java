47036cb85819d0ebc3a75359b4dcf57a8bd5efb8
Chetan Seth <chetan.seth2015@gmail.com>
2024-07-17 03:28:00 +0530

    adding Labs

--- src/ex_07072024/Lab005.java
+++ src/ex_07072024/Lab005.java
@@ -5,26 +5,21 @@ public class Lab005 {
 
     public static void main(String[] args) {
 
-        String str1 = "Hello";
-
-        String str2 = " Guys";
-
-        String str3 = "Hello Guys";
-
-        String str4 = str1.concat(str2);// Hello Guys
-
-        System.out.println(str1);
-        System.out.println(str2);
-        System.out.println(str3);
-        System.out.println(str4);
+        String str1 = "Hello"; // in SCP
+        String str2 = " Guys"; // in SCP
+        String str3 = "Hello Guys"; // in SCP
+        String str4 = str1.concat(str2);// in Heap
+        String str5 = "Hello" +" Guys"; // in SCP
+        String str6 = str1 + str2; // in Heap
 
 
-        //str1 will refer to "Hello" in SCP
-        //str2 will refer to "Guys" in SCP
-        //str3 and str4 both will refer to "Hello Guys" in SCP so only 1 new object will get created.
-        //key learning here is  that only 3 objects will be created in String constant pool.
+        System.out.println(str3 == str4); // False
+        System.out.println(str3 == str4.intern()); //True
+        System.out.println(str5 == str6); // false
+        System.out.println(str3 == str5); // true as both in scp
+        System.out.println(str4 == str6); // false, both in heap but differnt objects
     }
-
 }
+
 
 
