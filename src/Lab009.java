e102b68f316ffb99af58f25daac159e8088d0ba3
Chetan Seth <chetan.seth2015@gmail.com>
2024-07-20 22:49:04 +0530

    adding Labs

---
+++ src/Lab009.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Lab009 {
+
+    public static void main(String[] args)
+
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String= ");
+        String str= sc.next();
+        String nstr="";
+
+        for(int i=0;i<str.length();i++)
+        {
+            char ch =str.charAt(i);
+            nstr= ch+ nstr;
+        }
+
+        if (nstr.equals(str))
+
+            System.out.println("Yes,Palindrome String");
+
+        else
+            System.out.println("Not a Palindrome String");
+
+        System.out.println(nstr);
+    }
+
+}
+
+
